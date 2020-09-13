baf41690aa745a37fab4e1f772ea59fd917df627
Padmanabhan <24695860+prathick96@users.noreply.github.com>
2020-09-13 19:26:03 +0100

    port changes

--- src/index.js
+++ src/index.js
@@ -22,6 +22,6 @@ app.use("/posts", postRouter);
 app.use("/authors", authorRouter);
 app.use("/admin", adminRouter);
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log("Server started");
 });
